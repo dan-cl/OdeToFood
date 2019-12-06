f3f17cdab05d6b1a2aa9ce94bb9802d88c0925da
Dan-Cl <Dan.Clissold@protonmail.com>
2019-12-06 14:02:47 +1100

    builds model for index controller

--- OdeToFood.Web/Controllers/HomeController.cs
+++ OdeToFood.Web/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OdeToFood.Data.Services;
 
 namespace OdeToFood.Web.Controllers
 {
     public class HomeController : Controller
     {
+        IRestaurantData db;
+
+        public HomeController()
+        {
+            db = new InMemoryRestaurantData();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var model = db.GetAll();
+            return View(model);
         }
 
         public ActionResult About()
