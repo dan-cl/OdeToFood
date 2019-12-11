9a05b9b46ef93c351ff765fe5eb549dcca16bfba
Dan-Cl <Dan.Clissold@protonmail.com>
2019-12-11 17:09:13 +1100

    Using MVC Models

---
+++ OdeToFood.Web/Api/RestaurantsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OdeToFood.Data.Models;
+using OdeToFood.Data.Services;
+
+namespace OdeToFood.Web.Api
+{
+    public class RestaurantsController : ApiController
+    {
+        private readonly IRestaurantData _db;
+
+        public RestaurantsController(IRestaurantData db)
+        {
+            this._db = db;
+        }
+        public IEnumerable<Restaurant> Get()
+        {
+            var model = _db.GetAll();
+            return model;
+        }
+    }
+}
--- OdeToFood.Web/Controllers/GreetingController.cs
+++ OdeToFood.Web/Controllers/GreetingController.cs
@@ -11,9 +11,10 @@ namespace OdeToFood.Web.Controllers
     public class GreetingController : Controller
     {
         // GET: Greeting
-        public ActionResult Index()
+        public ActionResult Index(string name)
         {
             var model = new GreetingViewModel();
+            model.name = name ?? "no name";
             model.message = ConfigurationManager.AppSettings["message"];
 
             return View(model);
