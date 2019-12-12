310b109858acf6f1cd8045faa97330fc8d8bfacd
Dan-Cl <Dan.Clissold@protonmail.com>
2019-12-12 18:11:12 +1100

    POST redirect GET

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -54,7 +54,7 @@ namespace OdeToFood.Web.Controllers
             if (ModelState.IsValid)
             {
                 _db.Add(restaurant);
-                return View();
+                return RedirectToAction("Details", new {id = restaurant.Id});
             }
 
             return View(); 
